7c8c7e4f8a68e319596700936eb82b1d4af94761
allwing12 <allwing1232@gmail.com>
2022-09-22 16:44:43 +0900

    엔티티 추가 및 리포지터리와 sql에 넣기

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -1,51 +1,52 @@
 package com.mysite.sbb;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 
 @Controller
 public class MainController {
     public static int i = 1;
 
-//    @RequestMapping("/sbb")
-//    @ResponseBody
-//    public String index() {
-//        System.out.println("sbb");
-//        return "sbb";
-//    }
 
-    @GetMapping("/plus")
+    @RequestMapping("/sbb")
     @ResponseBody
-    public int plus(int a, int b) {
-        return a+b;
+    public String index() {
+        System.out.println("sbb");
+        return "sbb";
+    }
+    @RequestMapping("/saveSessionAge")
+    @ResponseBody
+    public String saveSession (@RequestParam("age")int age, HttpSession session) {
+        session.setAttribute("age", age);
+        return "나이 %d이 세션에 저장 되었습니다.".formatted(age);
+    }
+    @RequestMapping("/getSessionAge")
+    @ResponseBody
+    public String getSession (HttpSession session, HttpServletResponse res) {
+        int age = (int) session.getAttribute("age");
+        Cookie cookie = new Cookie("age",String.valueOf(age));
+        res.addCookie(cookie);
+        return "세션에 저장된 나이는 %d입니다.".formatted(age);
     }
 
-
-    @GetMapping("/minus")
+    @GetMapping("/addPerson/{id}")
     @ResponseBody
-    public int minus(int a, int b) {
-        return a-b;
+    public Person addPerson(Person person) {
+        return person;
     }
+}
 
-    @GetMapping("/increase")
-    @ResponseBody
-    public int increase () {
-        if (i < 3){
-            ++i;
-        } else if (i <= 3) {
-            i = 1;
-        }
-        return i;
-    }
-
-    @GetMapping("gugudan")
-    @ResponseBody
-    public String gugudan (int dan, int limit) {
-        String result = "";
-        for(int i = 1; i <= limit; i++) {
-            result += dan + " * " + i + " = " + (dan * i) + "<br/>\n";
-        }
-        return result;
-    }
+@Getter
+@AllArgsConstructor
+class Person {
+    private int id;
+    private int age;
+    private String name;
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,32 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.question.dao.QuestionRepository;
+import com.mysite.sbb.question.domain.Question;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
 	void contextLoads() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q1.setSubject("sbb가 무엇알고싶은데?");
+		q1.setContent("sbb에 대해 궁금한데..");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);
 	}
 
 }
